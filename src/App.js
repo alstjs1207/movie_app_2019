735a2a12673919d966c2f71a1ecf9592b0f601e2
alstjs1207 <alstjs1207@naver.com>
2020-02-28 15:42:23 +0900

    4.5 css를 적용해 보자

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
           movies
         }
       }
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=like_count');
     //this.setState({movies:movies, isLoading: false})
     this.setState({movies, isLoading: false})
   };
@@ -25,21 +25,19 @@ class App extends React.Component {
   }
   render() {
     const {isLoading, movies} = this.state;
-    return (<section class="container">
+    return (<section className="container">
       {
         isLoading
-          ? (<div class="loader">
-            <span class="loader_text">Loading..</span>
+          ? (<div className="loader">
+            <span className="loader__text">Loading..</span>
           </div>)
-          : (<div class="movies">
-            {movies.map(movie => (<Movie key={movie.id}
-                                         id={movie.id}
-                                         year={movie.year}
-                                         title={movie.title}
-                                         summary={movie.summary}
-                                         poster={movie.medium_cover_image}/>
-            ))}
-          </div>)
+          : (<div><h1>Like</h1>
+            <div className="movies">
+            {movies.map(movie => (
+              <Movie key={movie.id} id={movie.id} year={movie.year}
+                title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}
+                rating={movie.rating}/>))}
+          </div></div>)
       }
     </section>);
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
-  return (<div class="movie">
-
-    <img src={poster} alt={title} titlt={title}></img>
-    <div class="movie__data">
-      <h3 class="movie__title">{title}</h3>
-      <h5 class="movie__year">{year}</h5>
-      <p class="movie__summary">{summary}</p>
+function Movie({id, year, title, summary, poster, genres, rating}) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} titlt={title}></img>
+    <div className="movie__data">
+      <h3 className="movie__title">{title}</h3>
+    <h4 className="movie__rating">{rating}/10</h4>
+      <h5 className="movie__year">{year}</h5>
+      <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+    <p className="movie__summary">{summary.slice(0,180)}...</p>
     </div>
   </div>)
 }
@@ -18,7 +27,9 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  rating: PropTypes.number.isRequired
 
 };
 
