59b76664f8c649b1399545f926aebe5ee70ff703
alstjs1207 <alstjs1207@naver.com>
2020-02-28 11:25:04 +0900

    4.2 style 정리해 보자.!

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,
+    isLoading: true,
     movies: []
   };
-  getMovies = async() => {
+  getMovies = async () => {
     //movies.data.data.movies == {data: {data: { movies } } }
-    const {data: {data: { movies } } } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    const {
+      data: {
+        data: {
+          movies
+        }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     //this.setState({movies:movies, isLoading: false})
     this.setState({movies, isLoading: false})
   };
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
-  render(){
+  render() {
     const {isLoading, movies} = this.state;
-    return (
-      <div>
-        {isLoading ? "Loading.." : movies.map(movie =>
-          {
-           return (<Movie
-                    key={movie.id}
-                    id={movie.id}
-                    year={movie.year}
-                    title={movie.title}
-                    summary={movie.summary}
-                    poster={movie.medium_cover_image} />
-                  );
-            })}
-      </div>
-    );
+    return (<section class="container">
+      {
+        isLoading
+          ? (<div class="loader">
+            <span class="loader_text">Loading..</span>
+          </div>)
+          : (<div class="movies">
+            {movies.map(movie => (<Movie key={movie.id}
+                                         id={movie.id}
+                                         year={movie.year}
+                                         title={movie.title}
+                                         summary={movie.summary}
+                                         poster={movie.medium_cover_image}/>
+            ))}
+          </div>)
+      }
+    </section>);
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -2,25 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Movie({id, year, title, summary, poster}) {
-  return (
-    <div>
-      <img src={poster}></img>
-      <div>
-        <h1>{title}</h1>
-      </div>
-      <div>
-        <h4>{summary}</h4>
-      </div>
+  return (<div class="movie">
+
+    <img src={poster} alt={title} titlt={title}></img>
+    <div class="movie__data">
+      <h3 class="movie__title">{title}</h3>
+      <h5 class="movie__year">{year}</h5>
+      <p class="movie__summary">{summary}</p>
     </div>
-  )
+  </div>)
 }
 
 Movie.propTypes = {
-  id : PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster : PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired
 
 };
 
