9019e3137f9ac42c35865668c1c81ed3914ba074
alstjs1207 <alstjs1207@naver.com>
2020-03-03 19:09:24 +0900

    Updates

--- precache-manifest.fbb15d7d1bf5b07557bb27b3cc5efebc.js
+++ precache-manifest.fbb15d7d1bf5b07557bb27b3cc5efebc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0b13869d0f37643894df6b7f9d32331",
+    "revision": "1c6bedc3d9c92e74c8cf735d256f5957",
     "url": "/movie_app_2019/index.html"
   },
   {
-    "revision": "3f972e24dc205240e697",
+    "revision": "4742346a4f8965a1291d",
     "url": "/movie_app_2019/static/css/main.d2ab3d53.chunk.css"
   },
   {
-    "revision": "074e09439d2a13960454",
-    "url": "/movie_app_2019/static/js/2.40083667.chunk.js"
+    "revision": "d50daf797cca7887afcb",
+    "url": "/movie_app_2019/static/js/2.0601d884.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/movie_app_2019/static/js/2.40083667.chunk.js.LICENSE"
+    "url": "/movie_app_2019/static/js/2.0601d884.chunk.js.LICENSE"
   },
   {
-    "revision": "3f972e24dc205240e697",
-    "url": "/movie_app_2019/static/js/main.c50eab74.chunk.js"
+    "revision": "4742346a4f8965a1291d",
+    "url": "/movie_app_2019/static/js/main.cb622818.chunk.js"
   },
   {
     "revision": "101d7acd549509bcf93b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2019/precache-manifest.14f03ddf24d12c2e4ca59896fcaad959.js"
+  "/movie_app_2019/precache-manifest.fbb15d7d1bf5b07557bb27b3cc5efebc.js"
 );
 
 self.addEventListener('message', (event) => {
